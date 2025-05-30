0cc88aaf7a421649e3a8af31f45ac467149c30d9
nxoim <ghite@nxoim.com>
2025-05-30 22:39:12 +0200

    Added IndexBasedTiler overloads that help create tilers with less boilerplate. They are based on GenericTiler, another new abstraction. GenericTiler handles the creation of tiler state flow and changes in configurations

---
+++ library/tiler/src/commonMain/kotlin/com/tunjid/tiler/tilers/GenericTiler.kt
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tunjid.tiler.tilers
+
+import com.tunjid.tiler.ListTiler
+import com.tunjid.tiler.PivotRequest
+import com.tunjid.tiler.QueryFetcher
+import com.tunjid.tiler.Tile
+import com.tunjid.tiler.TiledList
+import com.tunjid.tiler.concurrentListTiler
+import com.tunjid.tiler.listTiler
+import com.tunjid.tiler.tiledListOf
+import com.tunjid.tiler.toPivotedTileInputs
+import com.tunjid.tiler.toTiledList
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flattenConcat
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+
+interface GenericTiler<Query : Any, Item> {
+    /**
+     * State flow of all relevant tiles. Initial value is an empty list.
+     */
+    val relevantTiles: StateFlow<TiledList<Query, Item>>
+
+    /**
+     * Tiler will update all relevant and irrelevant data
+     * in its's cache based on the most relevant query set.
+     */
+    fun setMostRelevantQuery(to: Query)
+}
+
+class GenericTilerConfiguration<T>(
+    val activeCachedAmount: Int,
+    val inactiveCachedAmount: Int,
+    val itemSizeHint: Int? = null,
+    val comparator: Comparator<T>
+)
+
+internal class GenericTilerImpl<Query : Any, Item>(
+    private val coroutineScope: CoroutineScope,
+    private val initial: Query,
+    private val configuration: Flow<GenericTilerConfiguration<Query>>,
+    private val fetcher: QueryFetcher<Query, Item>,
+    private val onNextQueryRequest: (sourceQuery: Query) -> Query,
+    private val onPreviousQueryRequest: (sourceQuery: Query) -> Query?,
+) : GenericTiler<Query, Item> {
+    private val mostRelevantQuery = MutableStateFlow<Query>(initial)
+
+    override val relevantTiles = createGenericTiledListFlow(
+        initial = initial,
+        mostRelevantQuery = mostRelevantQuery,
+        configuration = configuration,
+        fetcher = fetcher,
+        nextQuery = onNextQueryRequest,
+        previousQuery = onPreviousQueryRequest,
+        listTilerBuilder = ::listTiler
+    ).stateIn(coroutineScope, SharingStarted.WhileSubscribed(), tiledListOf())
+
+    override fun setMostRelevantQuery(to: Query) {
+        mostRelevantQuery.update { to }
+    }
+}
+
+internal class GenericConcurrentTilerImpl<Query : Any, Item>(
+    private val coroutineScope: CoroutineScope,
+    private val initial: Query,
+    configuration: Flow<GenericTilerConfiguration<Query>>,
+    fetcher: QueryFetcher<Query, Item>,
+    onNextQueryRequest: (sourceQuery: Query) -> Query,
+    onPreviousQueryRequest: (sourceQuery: Query) -> Query?,
+) : GenericTiler<Query, Item> {
+    private val mostRelevant = MutableStateFlow<Query>(initial)
+
+    override val relevantTiles = createGenericTiledListFlow(
+        initial = initial,
+        mostRelevantQuery = mostRelevant,
+        configuration = configuration,
+        fetcher = fetcher,
+        nextQuery = onNextQueryRequest,
+        previousQuery = onPreviousQueryRequest,
+        listTilerBuilder = ::concurrentListTiler
+    )
+        .stateIn(coroutineScope, SharingStarted.WhileSubscribed(), tiledListOf())
+
+    override fun setMostRelevantQuery(to: Query) {
+        mostRelevant.update { to }
+    }
+}
+
+/**
+ * Combines all the requirements into one place and produces
+ * a [TiledList] flow
+ */
+private fun <Query : Any, Item> createGenericTiledListFlow(
+    initial: Query,
+    mostRelevantQuery: StateFlow<Query>,
+    configuration: Flow<GenericTilerConfiguration<Query>>,
+    fetcher: QueryFetcher<Query, Item>,
+    nextQuery: Query.() -> Query,
+    previousQuery: Query.() -> Query?,
+    listTilerBuilder: (
+        order: Tile.Order<Query, Item>,
+        limiter: Tile.Limiter<Query, Item>,
+        fetcher: QueryFetcher<Query, Item>,
+    ) -> ListTiler<Query, Item>
+): Flow<TiledList<Query, Item>> = configuration
+    .map { configuration ->
+        mostRelevantQuery
+            .toPivotedTileInputs<Query, Item>(
+                PivotRequest<Query, Item>(
+                    onCount = configuration.activeCachedAmount,
+                    offCount = configuration.inactiveCachedAmount,
+                    comparator = configuration.comparator,
+                    nextQuery = nextQuery,
+                    previousQuery = previousQuery
+                )
+            )
+            .toTiledList(
+                listTiler = listTilerBuilder(
+                    Tile.Order.PivotSorted(
+                        query = initial,
+                        comparator = configuration.comparator
+                    ),
+                    Tile.Limiter(
+                        maxQueries = configuration.activeCachedAmount,
+                        itemSizeHint = configuration.itemSizeHint
+                    ),
+                    fetcher
+                )
+            )
+    }
+    .flattenConcat()
---
+++ library/tiler/src/commonMain/kotlin/com/tunjid/tiler/tilers/IndexBasedTiler.kt
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tunjid.tiler.tilers
+
+import com.tunjid.tiler.QueryFetcher
+import com.tunjid.tiler.utilities.neighboredQueryFetcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+/**
+ * Covers tiling of data that can be fetched by their index from a source.
+ *
+ * Example:
+ * ```kotlin
+ * class YourModel {
+ *     private val tiler = IndexBasedTiler<YourItemType>(
+ *         coroutineScope = coroutineScope,
+ *         initialIndex = 0,
+ *         activeCachedAmount = 3 // at least 3,
+ *         inactiveCachedAmount = 3,
+ *         fetcher = { index ->
+ *             // this is a page of your items, Flow<List<YourItemType>>
+ *             yourSource.getPotentiallyUpdatablePageFlow(index)
+ *         }
+ *     )
+ *
+ *     val content = tiler.relevantTiles
+ *
+ *     // this will be called by your ui while scrolling
+ *     fun setMostRelevantPageIndex(index: Long) {
+ *         tiler.setMostRelevantQuery(index)
+ *     }
+ * }
+ * ```
+ *@param initialIndex The index tiling will start from
+ *
+ * @param activeCachedAmount The amount of concurrent queries to collect from at any one time. Must be at least 3
+ *
+ * @param inactiveCachedAmount The amount of queries to keep in memory, but not collect from
+ *
+ * @param itemSizeHint Optimizes retrieval speed for items fetched.
+ * Use only if you can guarantee that your queries return a fixed number
+ * of items each time, for example SQL queries with a limit parameter.
+ *
+ * @see neighboredQueryFetcher
+ */
+@Suppress("FunctionName")
+fun <T> IndexBasedTiler(
+    coroutineScope: CoroutineScope,
+    initialIndex: Long,
+    activeCachedAmount: Int,
+    inactiveCachedAmount: Int,
+    itemSizeHint: Int? = null,
+    fetcher: QueryFetcher<Long, T>,
+): GenericTiler<Long, T>  = IndexBasedTiler(
+    coroutineScope,
+    initialIndex,
+    flowOf(
+        IndexBasedTilerConfiguration(
+            activeCachedAmount = activeCachedAmount,
+            inactiveCachedAmount = inactiveCachedAmount,
+            itemSizeHint = itemSizeHint
+        )
+    ),
+    fetcher
+)
+
+/**
+ * Covers tiling of data that can be fetched by their index from a source.
+ * This overload accepts a flow of configurations for adapting to ui.
+ *
+ * Example:
+ * ```kotlin
+ * class YourModel {
+ *     private val gridColumns = MutableStateFlow(1)
+ *     private val tiler = IndexBasedTiler<YourItemType>(
+ *         coroutineScope = coroutineScope,
+ *         initialIndex = 0,
+ *         configuration = gridColumns.map {
+ *             IndexBasedTilerConfiguration(
+ *                 activeCachedAmount = (3 * it).coerceAtLeast(3), // at least 3
+ *                 inactiveCachedAmount = 1 * it
+ *             )
+ *         },
+ *         fetcher = { index ->
+ *             // this is a page of your items, Flow<List<YourItemType>>
+ *             yourSource.getPotentiallyUpdatablePageFlow(index)
+ *         }
+ *     )
+ *
+ *     val content = tiler.relevantTiles
+ *
+ *     // this will be called by your ui while scrolling
+ *     fun setMostRelevantPageIndex(to: Long) {
+ *         tiler.setMostRelevantQuery(to)
+ *     }
+ *
+ *     // this and other similar functions will be useful
+ *     // for changing the configuration of tiling
+ *     fun setVisibleGridColumns(to: Int) {
+ *         gridColumns.update { to }
+ *     }
+ * }
+ * ```
+ *@param initialIndex The index tiling will start from
+ *
+ * @see neighboredQueryFetcher
+ */
+@Suppress("FunctionName")
+fun <Item> IndexBasedTiler(
+    coroutineScope: CoroutineScope,
+    initialIndex: Long,
+    configuration: Flow<GenericTilerConfiguration<Long>>,
+    fetcher: QueryFetcher<Long, Item>,
+): GenericTiler<Long, Item> = GenericConcurrentTilerImpl<Long, Item>(
+    coroutineScope = coroutineScope,
+    initial = initialIndex,
+    configuration = configuration,
+    fetcher = fetcher,
+    onNextQueryRequest = { sourceQuery -> sourceQuery + 1 },
+    onPreviousQueryRequest = { sourceQuery ->  (sourceQuery - 1).takeIf { it >= 0 } },
+)
+
+/**
+ * @param activeCachedAmount The amount of concurrent queries to collect from at any one time. Must be at least 3
+ *
+ * @param inactiveCachedAmount The amount of queries to keep in memory, but not collect from
+ *
+ * @param itemSizeHint Optimizes retrieval speed for items fetched.
+ * Use only if you can guarantee that your queries return a fixed number
+ * of items each time, for example SQL queries with a limit parameter.
+ */
+@Suppress("FunctionName")
+fun IndexBasedTilerConfiguration(
+    activeCachedAmount: Int,
+    inactiveCachedAmount: Int,
+    itemSizeHint: Int? = null
+) = GenericTilerConfiguration(
+    activeCachedAmount = activeCachedAmount,
+    inactiveCachedAmount = inactiveCachedAmount,
+    comparator = Comparator<Long>(Long::compareTo),
+    itemSizeHint = itemSizeHint
+)
+
